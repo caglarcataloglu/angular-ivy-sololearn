6a109c0e6f67f2899df591ef95b0c39c128de26a
crozwise <caglarcataloglu@gmail.com>
2022-03-08 23:05:44 +0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,19 @@ import { Car } from './car';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
+  subaru: Car = { make: 'Subaru', model: 'Outback', miles: 58232 };
+  honda: Car = { make: 'Honda', model: 'Accord', miles: 39393 };
+  bmw: Car = { make: 'BMW', model: 'X3', miles: 4400 };
+  cars: Car[] = [this.subaru, this.honda, this.bmw];
 
-  subaru: Car = {make: 'Subaru', model: 'Outback', miles: 58232};
-honda: Car = {make: 'Honda', model: 'Accord', miles: 39393};
-bmw: Car = {make: 'BMW', model: 'X3', miles: 4400};
-cars:Car[ ] = [this.subaru, this.honda, this.bmw];
-  
+  prefix = 'I am a ';
+  name = 'star student';
 
-  prefix= "I am a ";
-  name= "star student";
-
-  speak(){
-    const sentence=this.prefix+this.name
+  speak() {
+    const sentence = this.prefix + this.name;
     return sentence;
   }
 }
