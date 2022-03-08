627a899a6d3fb8f1d3a9a29ddb4086e6b0de295e
crozwise <caglarcataloglu@gmail.com>
2022-03-09 00:00:22 +0300

    1 file added

---
+++ src/app/transportation.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Car } from './car';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TransportationService {
+  // this is where we will recreate our array of Car types
+  subaru: Car = {make:'Subaru', model: 'Outback', miles: 55500};
+  honda: Car = {make: 'Honda', model: 'Accord', miles: 39393};
+  bmw: Car = {make: 'BMW', model: 'X3', miles: 4400};
+
+  cars:Car[] = [this.subaru, this.honda, this.bmw];
+  constructor() {}
+
+  getCars(){
+    return this.cars;
+  }
+  // this is where we will write methods to export our Car array
+}
