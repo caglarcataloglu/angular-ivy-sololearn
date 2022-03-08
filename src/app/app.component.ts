7ad6948c9346fb313eeaab7bda9c0ed057df89bd
crozwise <caglarcataloglu@gmail.com>
2022-03-09 00:32:40 +0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,18 @@
 import { Component, VERSION } from '@angular/core';
 import { Car } from './car';
-import { TransportationService} from './transportation.service';
+import { TransportationService } from './transportation.service';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  
   cars: Car[];
-  constructor(private transportationService: TransportationService){
-  this.cars=this.transportationService.getCars()
+  constructor(private transportationService: TransportationService) {
+    this.cars = this.transportationService.getCars();
   }
 
-    name = 'star student';
+  name = 'star student';
 
   speak() {
     const sentence = this.name;
@@ -24,6 +23,10 @@ export class AppComponent {
   }
   counter = 0;
   increment() {
-  this.counter++;
-}
+    this.counter++;
+  }
+  phrase = "It's going";
+  update() {
+    this.phrase += ' ..and going';
+  }
 }
