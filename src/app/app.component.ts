d4937d1d3b688b99caaaa50ba7723544e766b752
crozwise <caglarcataloglu@gmail.com>
2022-03-08 23:05:22 +0300

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component, VERSION } from '@angular/core';
-
+import { Car } from './car';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+
+  subaru: Car = {make: 'Subaru', model: 'Outback', miles: 58232};
+honda: Car = {make: 'Honda', model: 'Accord', miles: 39393};
+bmw: Car = {make: 'BMW', model: 'X3', miles: 4400};
+cars:Car[ ] = [this.subaru, this.honda, this.bmw];
   
 
   prefix= "I am a ";
